c416c9b3b655125000031979c7d1d2b384956a6f
doan15 <146705572+doan15@users.noreply.github.com>
2025-06-09 23:13:48 +0700

    Bỏ chat Page

--- src/pages/Home/ChatMessages/ChatPage.jsx
+++ src/pages/Home/ChatMessages/ChatPage.jsx
@@ -1,56 +1,5 @@
-import { Box } from "@mui/material";
-import ContactList from "./ContactList";
-import ChatArea from "./ChatArea";
-import { useChat } from "./hooks/useChat";
+import React from "react";
 
-export default function ChatApp() {
-  const {
-    contacts,
-    setContacts,
-    selectedContact,
-    setSelectedContact,
-    currentContact,
-    currentMessages,
-    handleSendMessage,
-    isLoading,
-    isError,
-    errorMessage,
-  } = useChat();
-
-  // Thêm xử lý trạng thái loading và error
-  if (isLoading) {
-    return <Box>Đang tải...</Box>;
-  }
-
-  if (isError) {
-    return <Box>Lỗi: {errorMessage}</Box>;
-  }
-
-  return (
-    <Box
-      sx={{
-        position: "fixed",
-        top: "64px",
-        left: 0,
-        right: 0,
-        height: "calc(100vh - 64px)",
-        display: "flex",
-        flexDirection: "row",
-        zIndex: 1100,
-        overflow: "hidden",
-      }}
-    >
-      <ContactList
-        contacts={contacts}
-        selectedContact={selectedContact}
-        onContactSelect={setSelectedContact}
-        onContactUpdate={setContacts}
-      />
-      <ChatArea
-        contact={currentContact}
-        messages={currentMessages}
-        onSendMessage={handleSendMessage}
-      />
-    </Box>
-  );
+export default function ChatPage() {
+  return <div>ChatPage</div>;
 }
